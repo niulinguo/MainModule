a6f5067f09b59af550080c99474dacbc1575f7e7
Niles <niulinguo@163.com>
2018-10-29 16:45:13 +0800

    Initial commit

---
+++ app/src/main/java/com/niles/mainmodule/MyApp.java
@@ -0,0 +1,11 @@
+package com.niles.mainmodule;
+
+import android.app.Application;
+
+/**
+ * Created by Niles
+ * Date 2018/10/29 13:31
+ * Email niulinguo@163.com
+ */
+public class MyApp extends Application {
+}
